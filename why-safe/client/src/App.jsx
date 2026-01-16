b5d7332da01bf4e776d36bdc3c1fc7bd5fdc295c
arbabkhan0 <arbabkhanshab@gmnail.com>
2026-01-17 00:55:59 +0530

    Add gitignore and initial project setup

---
+++ why-safe/client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl text-green-400 font-bold">
+        WHY-SAFE is working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
